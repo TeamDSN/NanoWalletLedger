60af576d3f3e25d5e465bb4b0165246195847a1f
Son Nguyen <son.nguyen@fdssoft.com>
2019-04-08 14:33:09 +0700

    [feature-2.4.4-add-ledger] Add carlo check

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const path = require("path");
 
 (async () => {
   const app = await carlo.launch({});
+  app.mainWindow().maximize();
+
+  // Used to check if browser is started with carlo
+  await app.exposeFunction('carlo', () => true);
+
   app.on("exit", () => process.exit());
   app.serveFolder(path.join(__dirname, "build"));
   await app.load("start.html");
