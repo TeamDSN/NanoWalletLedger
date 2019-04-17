4d74762fb020fa2e3df56738bfcebc42176893cb
Long Lee <lethlong79@gmail.com>
2019-04-17 17:13:36 +0700

    Unit test for Ledger

---
+++ tests/specs/ledger.spec.js
@@ -0,0 +1,11 @@
+// const Ledger = require('../../src/app/modules/ledger/ledger.service.js');
+import ledgerService from "../../src/app/modules/ledger/ledger.service";
+
+describe("Ledger service", function() {
+    it ("Create bip44 path for testnet NetWork", function() {
+      //expect("44'/43'/152'/0'/0'").toBe(_Ledger.bip44(-104, 0));
+    });
+
+});
+
+   
