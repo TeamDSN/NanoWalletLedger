5b68e070181087c23072339db870eaa19fc00ef8
Long Lee <lethlong79@gmail.com>
2019-04-18 17:22:03 +0700

    Unit test Ledger-service(demo)

--- tests/specs/ledger.spec.js
+++ tests/specs/ledger.spec.js
@@ -1,11 +1,24 @@
-// const Ledger = require('../../src/app/modules/ledger/ledger.service.js');
 import ledgerService from "../../src/app/modules/ledger/ledger.service";
 
 describe("Ledger service", function() {
-    it ("Create bip44 path for testnet NetWork", function() {
-      //expect("44'/43'/152'/0'/0'").toBe(_Ledger.bip44(-104, 0));
-    });
+  let ledger;
+  beforeEach(angular.mock.module('app'));
 
+  beforeAll(function() {
+    ledger = new ledgerService;
+  });
+
+  it ("Create bip44 path for testnet NetWork", function() {
+    expect("44'/43'/152'/0'/0'").toBe(ledger.bip44(-104, 0));
+  });
+
+  it ("Create bip44 path for main NetWork", function() {
+    expect("44'/43'/104'/0'/0'").toBe(ledger.bip44(104, 0));
+  });
+
+  it ("Create an account", function() {
+    
+  });
 });
 
    
